2fcbc4dcf6f4902f58974d304565f148d71f5914
Wilsongunde <gundewilson.123@gmail.com>
2020-10-26 11:41:38 +0530

    Added the screen and nobroker classes

---
+++ src/main/java/com/nobroker/screens/screen.java
@@ -0,0 +1,13 @@
+package com.nobroker.screens;
+
+public class screen {
+
+	public static void main(String arg[])
+	{
+		
+		
+		System.out.println("screen");
+		
+	}
+	
+}
---
+++ src/test/java/com/nobroker/Test/nobroker.java
@@ -0,0 +1,11 @@
+package com.nobroker.Test;
+
+public class nobroker {
+
+	public static void main(String[] args) {
+		
+		System.out.println("no broker");
+
+	}
+
+}
